24506e2916b276d1e3aa23a924fb3386b7e3e438
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2018-10-01 15:05:38 +0300

    Add comments and a proper exit code at Exercise 4, and format the source files.

--- s1/procedural-programming/a12f1.c
+++ s1/procedural-programming/a12f1.c
@@ -1,13 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 
-#define TWO_MONTHS 61.0/365.0
+#define TWO_MONTHS 61.0 / 365.0
 
-void calculateIt (unsigned long kathara, unsigned long mikta, double* dt, double* df) {
-    *dt = (double) kathara * 1.33 * TWO_MONTHS;
-    *df = (double) mikta * 0.13 * TWO_MONTHS;
+void calculateIt(unsigned long kathara, unsigned long mikta, double *dt, double *df)
+{
+    *dt = (double)kathara * 1.33 * TWO_MONTHS;
+    *df = (double)mikta * 0.13 * TWO_MONTHS;
 }
 
-int main() {
+int main()
+{
     unsigned long kathara, mikta;
     double dt, df;
 
@@ -16,7 +18,7 @@ int main() {
     printf("Dose ta mikta tetragonika: ");
     scanf("%u", &mikta);
 
-    calculateIt (kathara, mikta, &dt, &df);
+    calculateIt(kathara, mikta, &dt, &df);
 
     printf("Ta dimotika teli einai: %0.2f\n", dt);
     printf("Ta mikta teli einai: %0.2f\n", df);
--- s1/procedural-programming/a4f1.c
+++ s1/procedural-programming/a4f1.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 
-void calculateIt(long amount, long profitRate, double* profit, double* total) {
+/* We cannot return two values from a function, so it has to be done the pointer way. */
+void calculateIt(long amount, long profitRate, double *profit, double *total)
+{
     *profit = amount * profitRate / 100.0;
-    *total = (double) amount + *profit;
+    *total = (double)amount + *profit;
 }
 
-main() {
+int main()
+{
     long amount, profitRate;
     double profit, total;
 
+    /* INPUT */
     printf("Give the total amount: ");
     scanf("%d", &amount);
     printf("Give the profit rate: ");
     scanf("%d", &profitRate);
 
+    /* COMPUTATION */
     calculateIt(amount, profitRate, &profit, &total);
 
+    /* OUTPUT */
     printf("The profit is %0.2f and the total sale amount is %0.2f.\n", profit, total);
 
+    /* Code::Blocks displays that "Process terminated with exit code <something other than zero>." */
+    return 0;
 }

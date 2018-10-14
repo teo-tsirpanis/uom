3cada1b34aed7177971d6d9c45d36206242ee8ed
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2018-10-14 21:05:31 +0300

    Update some comments in Exercise 20.

--- s1/procedural-programming/a20f2.c
+++ s1/procedural-programming/a20f2.c
@@ -5,7 +5,9 @@
 jump assembly instructions, improving performance.
 As shown in https://godbolt.org/z/Ar4xI3, GCC yields only two
 "jle" instructions, but they are for the purpose of the assertion.
-When compiled with "-O3", there is only one "ja" instruction. */
+When compiled with "-O3", there is only one "ja" instruction.
+So, there is just one jump instruction instead of seven, and the CPU's branch
+predictor would have much less opportunities to adversely affect performance. */
 static char *daysLookup[7] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 
 char *getDay(int num)

23abcb7ecdd1c20f8cdabbfd92b9b4661931a0f4
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2018-10-24 23:23:07 +0300

    Conform Exercise 5 to its statement by calculating the faux-Armstrong numbers. And fix a mysterious bug that needs to be researched further.

--- s1/procedural-programming/a5f4.c
+++ s1/procedural-programming/a5f4.c
@@ -1,17 +1,33 @@
+#ifndef REAL_ARMSTRONG_NUMBERS
+#include <assert.h>
+#endif
 #include <math.h>
 #include <stdio.h>
 
 /* Per Wikipedia an Armstrong Number is a number that is the sum of
 its own digits each raised to the power of the number of digits.
-Which means that */
-int isArmstrong (unsigned int x)
+Which means that. But the exercise does not want us to do that.
+Uncomment the following line for the actual definition of the Armstrong Numbers. */
+
+/* #define REAL_ARMSTRONG_NUMBERS */
+
+#define cube(x) pow(x, 3)
+
+int isArmstrong(unsigned int x)
 {
-    unsigned int digitsCount = ceil(log10(x));
     unsigned int sum = 0;
+#ifdef REAL_ARMSTRONG_NUMBERS
+    unsigned int digitsCount = ceil(log10(x));
+#else
+    unsigned int digitsCount = 3;
+    assert(x <= 999);
+#endif
     unsigned int i = x;
     while (i)
     {
-        sum += pow(i % 10, digitsCount);
+        /* The omission of the floor would make 5^3 equal to 124.
+        TODO: Ask Stack Overflow about that. */
+        sum += floor(pow(i % 10, digitsCount));
         i /= 10;
     }
     return sum == x;

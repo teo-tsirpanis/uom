33f5931cfdf8be095d5a187b8f3a574e4e372006
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2018-11-02 17:56:31 +0200

    Complete Exercise 4.

--- s1/procedural-programming/a4f5.c
+++ s1/procedural-programming/a4f5.c
@@ -1,22 +1,41 @@
 #include <stdio.h>
 
 int sales[4][5] =
-{
-    {10, 4, 5, 6, 7},
-    {7, 0, 12, 1, 3},
-    {4, 9, 5, 0, 8},
-    {3, 2, 1, 5, 6}
-};
+    {
+        {10, 4, 5, 6, 7},
+        {7, 0, 12, 1, 3},
+        {4, 9, 5, 0, 8},
+        {3, 2, 1, 5, 6}};
 
 int prices[5] = {25000, 15000, 32000, 21000, 9200};
 
 int main()
 {
     int incomePerEmployee[4] = {0, 0, 0, 0};
-    int i;
+    int i, j;
+    int quantities[5] = {0, 0, 0, 0, 0};
+
     for (i = 0; i < 4; i++)
         for (j = 0; j < 5; j++)
-            incomePerEmployee[i] += sales[i][j] + prices[j];
-    printf("Synoliko Poso Eispraksis / Pwlhth: %d %d %d %d\n", incomePerEmployee[0], incomePerEmployee[1], incomePerEmployee[2], incomePerEmployee[3]);
-    printf("Synoliko");
+        {
+            quantities[j] += sales[i][j];
+            incomePerEmployee[i] += sales[i][j] * prices[j];
+        }
+
+    printf("Synoliko Poso Eispraksis / Pwlhth:");
+    for (i = 0; i < 4; i++)
+        printf(" %d", incomePerEmployee[i]);
+    printf("\n");
+
+    printf("Promitheia / Pwlhth:");
+    for (i = 0; i < 4; i++)
+        printf(" %.2f", (float)incomePerEmployee[i] / 10.0);
+    printf("\n");
+
+    printf("Posothtes Proiontwn:");
+    for (i = 0; i < 5; i++)
+        printf(" %d", quantities[i]);
+    printf("\n");
+
+    return 0;
 }

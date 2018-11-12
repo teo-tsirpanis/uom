ba0b6f5c89333a6840bade3e6d4c1ed56222faf5
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2018-11-12 16:47:51 +0000

    Complete Exercise 6.2 (needs testing).

--- s1/procedural-programming/a2f6.c
+++ s1/procedural-programming/a2f6.c
@@ -3,17 +3,37 @@
 int GetInteger();
 
 #define PROMPT() printf("? "), GetInteger()
+#define MAX_ELEMENTS 100
+
+int inputArray(int data[MAX_ELEMENTS])
+{
+    int i = 0;
+    do
+    {
+        data[i++] = PROMPT();
+    } while (data[i] != -1 && i < MAX_ELEMENTS)
+    return --i;
+}
+
+void minMax(int length, int data[length], int *min, int *max)
+{
+    *min = *max = data[0]
+    for (int i = 1; i < length; i++)
+    {
+        *min = data[i] < *min ? data[i]: *min;
+        *max = data[i] > *max ? data[i]: *max;
+    }
+}
 
 int main()
 {
     printf("Enter the elements of the array, one per line.\n");
     printf("Use -1 to signal the end of the list.\n");
 
+    int data[MAX_ELEMENTS], length = inputArray(data);
     int min, max;
+    minMax(length, data, &min, &max);
 
-    int i = PROMPT();
-    min = max = i;
-
-    
+    printf("The range of values is %d-%d", min, max);
     return 0;
 }

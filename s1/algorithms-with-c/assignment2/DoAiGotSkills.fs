106ee41b19f93bbb39b7d62de3d67cca35daafa0
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2018-12-04 14:06:08 +0200

    Add a more complete F# program.

--- s1/algorithms-with-c/assignment2/DoAiGotSkills.fs
+++ s1/algorithms-with-c/assignment2/DoAiGotSkills.fs
@@ -1,10 +1,20 @@
 // © by Theodore Tsirpanis
+
+open FsCheck
+open System
+open System.Diagnostics
+
+type ProblemDomain = {
+    Employees: (int * int Set) list
+    AllSkills: int Set
+} 
+
 let tryMax =
     function
     | [] -> None
     | x -> List.max x |> Some
 
-let solveIt employees allSkills =
+let solveIt {Employees = employees; AllSkills = allSkills} =
     let rec impl hired skills =
         function
         | _ when skills = allSkills -> Some hired
@@ -17,6 +27,14 @@ let solveIt employees allSkills =
             |> List.choose id
             |> tryMax
     impl Set.empty Set.empty employees
+    
+let problemDomainGen employees skills =
+    Gen.choose (1, skills)
+    |> Gen.nonEmptyListOf
+    |> Gen.map set
+    |> Gen.listOfLength employees
+    |> Gen.map (List.mapi (fun idx x -> idx + 1, x))
+    |> Gen.map (fun x -> {Employees = x; AllSkills = set [1 .. skills]})
 
 let sample1 =
     [
@@ -30,3 +48,27 @@ let sample1 =
     |> List.mapi (fun idx x -> idx + 1, set x)
 
 let allSkills1 = set [1 .. 6]
+
+let tryGetInt def args idx =
+    let impl x =
+        let mutable v = 0
+        if x <> "" && Int32.TryParse(x, &v) then
+            Some v
+        else
+            None
+    Array.tryItem idx args |> Option.defaultValue "" |> impl |> Option.defaultValue def
+
+[<EntryPoint>]
+let main args =
+    let employees = tryGetInt 6 args 0
+    let skills = tryGetInt employees args 1
+    printfn "%d skills are needed, and there are %d employees available..." skills employees
+    let sw = Stopwatch()
+    let domain = Gen.eval skills (Random.newSeed()) (problemDomainGen employees skills)
+    printfn "%A" domain
+    sw.Start()
+    let result = solveIt domain
+    sw.Stop()
+    printfn "Solution: %A" result
+    printfn "Elapsed time: %O" sw.Elapsed
+    0

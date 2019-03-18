b6d95b8bb8f034137a06bc928dd31c735c2ef843
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2019-03-18 19:13:23 +0200

    Add the search function from Exercise 4.1.

--- s2/data-structures/a1f4.c
+++ s2/data-structures/a1f4.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include "adt-libraries/L_ListADT.c"
 
-void Search(ListPointer freePtr,
+void Search(ListPointer freePtr, // UNUSED
             ListPointer list,
-            NodeType Node[NumberOfNodes],
-            ListElementType item,
+            NodeType nodes[NumberOfNodes],
+            ListElementType itemToFind,
             boolean *found,
-            ListPointer *predPtr)
+            ListPointer *prevPtr)
 {
-
+    *prevPtr = NilValue;
+    ListPointer n = nodes[*prevPtr].Next;
+    while (n != NilValue) {
+        if (nodes[n].Data == itemToFind)
+        {
+            *found = TRUE;
+            return;
+        }
+        *prevPtr = n;
+        n = nodes[*prevPtr].Next;
+    }
 }

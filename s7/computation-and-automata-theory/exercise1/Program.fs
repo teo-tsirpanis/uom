8701c516f8acf327f3f838b10d56d8bfd1986fb4
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2019-10-22 11:31:34 +0000

    Complete the finite automata decider.

--- s7/computation-and-automata-theory/exercise1/Program.fs
+++ s7/computation-and-automata-theory/exercise1/Program.fs
@@ -17,6 +17,11 @@ type FAState = {
     Edges: Map<char, FAStateIndex Set>
     EpsilonTransitions: FAStateIndex Set
 }
+with
+    member x.GetNextStates c =
+        match x.Edges.TryGetValue(c) with
+        | true, states -> states
+        | false, _ -> Set.empty
 
 type FA = {
     InitialState: FAStateIndex
@@ -26,13 +31,35 @@ type FA = {
 let getState {States = fsStates} (FAStateIndex stateIdx) =
     fsStates.[stateIdx]
 
-let EClosure fa state =
+let EClosure fa states =
     let visited = HashSet()
-    let q = Queue()
-    q.Enqueue(getState fa state)
+    let q = Queue(states)
     while q.Count <> 0 do
         let state = q.Dequeue()
         if visited.Add(state) then
             state.EpsilonTransitions
             |> Set.iter (getState fa >> q.Enqueue)
     set visited
+
+let faAdvance fa states c =
+    states
+    |> EClosure
+    |> Seq.map (fun state -> state.GetNextStates(c))
+    |> Set.unionMany
+
+let faMatch fa str =
+    let dict = Dictionary()
+    let rec impl states i =
+        if i = String.length str then
+            states |> Set.exists (fun state -> state.IsAccept)
+        else
+            let newStates =
+                let c = str.[i]
+                match dict.TryGetValue((states, c)) with
+                | true, newStates -> newStates
+                | false, _ ->
+                    let newStates = faAdvance fa states c
+                    dict.Add((states, c), newStates)
+                    newStates
+            impl newStates (n + 1)
+    impl (Set.singleton fa.InitialState) 0

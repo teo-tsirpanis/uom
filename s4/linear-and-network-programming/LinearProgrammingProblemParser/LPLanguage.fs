a0942fb6c5f7c3a1e9fea83de5f3ca0af96edc1b
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2020-03-10 23:02:53 +0200

    OK, the second example works. I allowed the numbers to the RHS of a constraint to be signed. Looks like I forgot that.

--- s4/linear-and-network-programming/LinearProgrammingProblemParser/LPLanguage.fs
+++ s4/linear-and-network-programming/LinearProgrammingProblemParser/LPLanguage.fs
@@ -27,7 +27,7 @@ let designtime =
                 !& "+"             .>>. X => (fun     x -> Variable( 1,   x))
                 !& "-"             .>>. X => (fun     x -> Variable(-1,   x))
                 if allowNoLeadingSign then
-                    !@ number .>>. X => (fun num x -> Variable( num, x))
+                    !@ number .>>. X => (fun num x -> Variable(num, x))
             ]
         let firstExpression = mkProduction true  "First Expression"
         let moreExpressions = mkProduction false "More Expressions"
@@ -41,21 +41,27 @@ let designtime =
         !& "max" .>>. expression => Objective.Maximize
     ]
 
+    let signedNumber = "Signed Number" ||= [
+        !@ number => id
+        !& "+" .>>. number => id
+        !& "-" .>>. number => (~-)
+    ]
+
     let constraints =
         "Subject To" ||= [
-            !@ expression .>> "="  .>>. number => (curry Constraint.Equal)
-            !@ expression .>> "<=" .>>. number => (curry Constraint.LessThanOrEqual)
-            !@ expression .>> ">=" .>>. number => (curry Constraint.GreaterThanOrEqual)
+            !@ expression .>> "="  .>>. signedNumber => (curry Constraint.Equal)
+            !@ expression .>> "<=" .>>. signedNumber => (curry Constraint.LessThanOrEqual)
+            !@ expression .>> ">=" .>>. signedNumber => (curry Constraint.GreaterThanOrEqual)
         ] |> many1
 
-    let endMaybe =
-        "End Maybe" ||= [
-            !& "end" =% ()
-            empty =% ()
-        ]
+    let endMaybe = "End Maybe" ||= [
+        !& "end" =% ()
+        empty =% ()
+    ]
 
-    "Linear Programming Problem"
-        ||= [!@ objective .>> ST .>>. constraints .>> endMaybe => (fun o c -> {Objective = o; Constraints = c})]
+    "Linear Programming Problem" ||= [
+        !@ objective .>> ST .>>. constraints .>> endMaybe => (fun o c -> {Objective = o; Constraints = c})
+    ]
     // LINDO's comments are line-based and start with an exclamation mark.
     |> DesigntimeFarkle.addLineComment "!"
 
